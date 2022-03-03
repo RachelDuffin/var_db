63763c80d36ebedba1b1034b04001070c2492471
Rachel Duffin <rachel.g.duffin@gmail.com>
2022-03-03 10:53:27 +0000

    Add variant viewing functionality

--- vdb/urls.py
+++ vdb/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-	path('variants/', views.variantlist, name='variant_list')
-
+	path('variants/', views.variantlist, name='variant_list'),
+    path('variants/<int:pk>/', views.variantviewer, name='variant_viewer')
     ]
