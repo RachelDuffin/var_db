bcca80e26410efe6026864827b3348951364f8e4
Clodagh McGuire <clodagh.mcguire@nhs.net>
2022-03-03 10:13:55 +0000

    error fixing login page

--- vdb/views.py
+++ vdb/views.py
@@ -8,6 +8,5 @@ def variantlist(request):
     variants = Variant.objects.all()
     return render(request, 'vdb/variant_list.html', {'variants': variants})
 
-def login(request):
-    return render(request, 'vdb/login.html', {'login' : login})
+
 
